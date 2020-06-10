5222dc600414c87f15ad3582ff674c524b7ea234
MarkusTryban <markustryban@gmail.com>
2020-06-10 18:44:34 -0400

    Add buttons 1 - 9

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -3,8 +3,35 @@ import React from 'react';
 const CalcButton = () => {
   return (
     <div className='clac-layout'>
-      <button type='button' className='calc-button'>
+      <button id='zero' type='button' className='calc-button'>
         0
+      </button>
+      <button id='one' type='button' className='calc-button'>
+        1
+      </button>
+      <button id='two' type='button' className='calc-button'>
+        2
+      </button>
+      <button id='three' type='button' className='calc-button'>
+        3
+      </button>
+      <button id='four' type='button' className='calc-button'>
+        4
+      </button>
+      <button id='five' type='button' className='calc-button'>
+        5
+      </button>
+      <button id='six' type='button' className='calc-button'>
+        6
+      </button>
+      <button id='seven' type='button' className='calc-button'>
+        7
+      </button>
+      <button id='eight' type='button' className='calc-button'>
+        8
+      </button>
+      <button id='nine' type='button' className='calc-button'>
+        9
       </button>
     </div>
   );
