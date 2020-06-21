9a56c4d24463bc0ff07185c914dc7688533fe52e
MarkusTryban <markustryban@gmail.com>
2020-06-21 14:44:19 -0400

    Add isOperator to Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 export default class Button extends Component {
+  isOperator = (val) => {
+    return !isNaN(val) || val === '.' || val === '=';
+  };
+
   render() {
     return <div>{this.props.children}</div>;
   }
