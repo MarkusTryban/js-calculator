e2ca09cb8bfb68ab2deec2b81dc9bf52de9d07c8
MarkusTryban <markustryban@gmail.com>
2020-06-22 15:53:49 -0400

    Update ClearButton return

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class ClearButton extends Component {
   render() {
+    <div className='clear-btn' onClick={() => this.props.handleClear()}></div>;
     return <div>{this.props}</div>;
   }
 }
