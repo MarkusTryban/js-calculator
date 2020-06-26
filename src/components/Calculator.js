11d28d4b61a7005e40ce643b5273b4d4659f2888
MarkusTryban <markustryban@gmail.com>
2020-06-26 18:53:45 -0400

    Move CalcButton to Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -21,7 +21,35 @@ export default class Calculator extends Component {
         <div className='display'>
           <h1>{calcValue}</h1>
         </div>
-        <CalcButton />
+        <div className='wrapper'>
+          <div className='button-layout'>
+            <button id='seven'>7</button>
+            <button id='eight'>8</button>
+            <button id='nine'>9</button>
+            <button id='divide'>/</button>
+          </div>
+          <div className='button-layout'>
+            <button id='four'>4</button>
+            <button id='five'>5</button>
+            <button id='six'>6</button>
+            <button id='multiply'>*</button>
+          </div>
+          <div className='button-layout'>
+            <button id='one'>1</button>
+            <button id='two'>2</button>
+            <button id='three'>3</button>
+            <button id='add'>+</button>
+          </div>
+          <div className='button-layout'>
+            <button id='decimal'>.</button>
+            <button id='zero'>0</button>
+            <button id='equals'>=</button>
+            <button id='subtract'>-</button>
+          </div>
+          <div className='button-layout'>
+            <button id='clear'>clear</button>
+          </div>
+        </div>
         <div className='footer'>
           by{' '}
           <a
