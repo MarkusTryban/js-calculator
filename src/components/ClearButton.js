9c49aee3d1e6690af13a08c75c9da6ce3fa965a3
MarkusTryban <markustryban@gmail.com>
2020-06-28 16:15:18 -0400

    Add children to return props

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 export default class ClearButton extends Component {
   render() {
     <div className='clear-btn' onClick={() => this.props.handleClear()}></div>;
-    return <div>{this.props}</div>;
+    return <div>{this.props.children}</div>;
   }
 }
