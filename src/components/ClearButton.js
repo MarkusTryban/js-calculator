c76fa8ce62ebf88041ce8fe2b63f6eab5f5e5c18
MarkusTryban <markustryban@gmail.com>
2020-07-01 21:07:45 -0400

    Add element propType for children

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ClearButton = () => {
+const ClearButton = ({ children }) => {
   return (
-    <div className='clear-btn' onClick={() => this.props.handleClear()}>
-      {this.props.children}
+    <div
+      className='clear-btn'
+      role='button'
+      onClick={() => this.props.handleClear()}
+    >
+      {children}
     </div>
   );
+};
+
+ClearButton.propTypes = {
+  children: PropTypes.element.isRequired,
 };
 
 export default ClearButton;
