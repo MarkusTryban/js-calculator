0c7608a09304ef125189ba4d07013e636290ad5c
MarkusTryban <markustryban@gmail.com>
2020-07-17 19:02:49 -0400

    Add onKeyPress to button element

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,6 +14,7 @@ class Button extends Component {
       <div
         className={`button ${this.isOperator(children) ? '' : 'operator'}`}
         role='button'
+        onKeyPress={this.handleKeyPress}
         onClick={() => handleClick(children)}
       >
         {children}
