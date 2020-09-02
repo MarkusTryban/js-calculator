4b137874371f5536503d0d11c110027737d0c291
MarkusTryban <markustryban@gmail.com>
2020-09-01 20:01:13 -0400

    Return button element

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-export default ({ onButtonClick, buttonKey }) => {
+import PropTypes from 'prop-types';
+
+const Button = ({ onButtonClick, buttonKey }) => {
   const handleClick = () => {
     onButtonClick(buttonKey);
   };
@@ -11,3 +13,10 @@ export default ({ onButtonClick, buttonKey }) => {
     </button>
   );
 };
+
+Button.propTypes = {
+  onButtonClick: PropTypes.func.isRequired,
+  buttonKey: PropTypes.string.isRequired,
+};
+
+export default Button;
